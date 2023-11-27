618d1a3026e9a75baef83a9b259d88e0c504c98b
namita0210 <namita008mtaids22@igdtuw.ac.in>
2023-11-27 08:49:35 +0530

    done with basics

--- tutorial.py
+++ tutorial.py
@@ -5,16 +5,15 @@ app = Flask(__name__)
 
 @app.route("/") # decorator which tells the application which URL should call the associated function
 def home():
-    return '''This is the home page
-'''
+    return '''This is the home page'''
 
 @app.route("/<name>") # dynamic URL
-def name(name):
+def user(name):
     return f"""Welcome {name}"""
 
-@app.route("/admin") # dynamic URL
+@app.route("/admin") 
 def admin():
-    return redirect(url_for("home")) # redirect to home page
+    return redirect(url_for("user", name = "FantasticBaby")) # redirect to home page
 
 if __name__ == "__main__":
     app.run(debug=True) # run the application in debug mode
