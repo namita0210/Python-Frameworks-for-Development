cb8b14635bd7e99c1f03c114726383265178eca0
namita0210 <namita008mtaids22@igdtuw.ac.in>
2023-11-27 08:40:50 +0530

    Removed html script from new_branch

--- tutorial.py
+++ tutorial.py
@@ -5,62 +5,7 @@ app = Flask(__name__)
 
 @app.route("/") # decorator which tells the application which URL should call the associated function
 def home():
-    return '''This is the home page <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Welcome</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 0;
-            background-color: #f7f7f7;
-        }
-        header {
-            background-color: #3498db;
-            color: white;
-            text-align: center;
-            padding: 2em 0;
-        }
-        .container {
-            max-width: 800px;
-            margin: 0 auto;
-            padding: 2em;
-        }
-        h1 {
-            font-size: 3em;
-            margin-bottom: 0.5em;
-        }
-        p {
-            font-size: 1.2em;
-            color: #333;
-            line-height: 1.6;
-        }
-        .cta-button {
-            display: inline-block;
-            padding: 1em 2em;
-            background-color: #3498db;
-            color: white;
-            text-decoration: none;
-            border-radius: 5px;
-            transition: background-color 0.3s ease;
-        }
-        .cta-button:hover {
-            background-color: #2980b9;
-        }
-    </style>
-</head>
-<body>
-    <header>
-        <h1>Welcome to Our Website</h1>
-    </header>
-    <div class="container">
-        <p>Thank you for visiting our website. We provide amazing services and top-notch quality.</p>
-        <a href="#" class="cta-button">Get Started</a>
-    </div>
-</body>
-</html>
+    return '''This is the home page
 '''
 
 @app.route("/<name>") # dynamic URL
